5d07a5a4beacc63bc2215ffb760a411155dbdd20
Pedro Antonio <pedro_arxp@hotmail.com>
2026-06-28 12:33:27 -0300

    feat: adiciona hardening com helmet e rate limit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import { apiLimiter } from "./middlewares/rateLimiter";
 import clienteRoutes from './routes/clienteRoutes';
 import produtoRoutes from './routes/produtoRoutes';
 import carrinhoRoutes from "./routes/carrinhoRoutes";
@@ -9,8 +11,10 @@ import { notFoundHandler, errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
 app.use(express.json());
+app.use(apiLimiter);
 
 app.use('/clientes', clienteRoutes);
 app.use('/produtos', produtoRoutes);
