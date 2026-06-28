fed0f2112843f7408da6c5eb56ed5a555ae0c71a
Pedro Antonio <pedro_arxp@hotmail.com>
2026-06-28 12:33:27 -0300

    feat: adiciona hardening com helmet e rate limit

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,12 +3,13 @@ import authController from "../controllers/authController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { roleMiddleware } from "../middlewares/roleMiddleware";
 import { validate } from "../middlewares/validate";
+import { loginLimiter } from "../middlewares/rateLimiter";
 import { registerValidator, loginValidator } from "../validators/authValidator";
 
 const router = Router();
 
 router.post('/register', validate(registerValidator), authController.register);
-router.post('/login', validate(loginValidator), authController.login);
+router.post('/login', loginLimiter, validate(loginValidator), authController.login);
 router.get('/profile', authMiddleware, authController.profile)
 router.get('/admin', authMiddleware, roleMiddleware('ADMIN'), (req, res) => { return res.json({message: 'Area administrativa'});});
 
